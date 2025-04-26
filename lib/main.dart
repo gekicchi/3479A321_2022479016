016eeec8972fcf7e0437990d7c4a146ecc85febc
gekicchi <oyarzunm18@gmail.com>
2025-04-26 17:47:45 -0400

    lab 5 punto 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:logger/logger.dart';
+import 'pages/my_home_page.dart';
 
 const String catIcon =
     'C:/Users/oyarz/Desktop/moviles/3479A321_2022479016/assets/icons/cat_paw.svg';
 
+var logger = Logger(
+  printer: PrettyPrinter(),
+);
+
+var loggerNoStack = Logger(
+    printer: PrettyPrinter(
+  methodCount: 0,
+));
+
 void main() {
+  print(
+      'Run with either `dart example/main.dart` or `dart --enable-asserts example/main.dart`´.');
+  demo();
   runApp(const MyApp());
+}
+
+void demo() {
+  logger.d('Log message with 2 methods');
+
+  loggerNoStack.i('Info message');
+
+  loggerNoStack.w('Just a warning!');
+
+  logger.e('Error! Something bad happened', error: 'Test Error');
+
+  loggerNoStack.t({'key': 5, 'value': 'something'});
+
+  Logger(printer: SimplePrinter(colors: true)).t('boom');
 }
 
 class MyApp extends StatelessWidget {
@@ -19,79 +47,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Hola mundo'),
+      home: const MyHomePage(title: 'Home Page'),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  void _decrementCounter() {
-    setState(() {
-      _counter--;
-    });
-  }
-
-  void _restartCounter() {
-    setState(() {
-      _counter = 0;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      //persistentFooterButtons: botonesDePie,
-      body: Card(
-        color: Colors.brown,
-        shadowColor: Colors.orange,
-        margin: const EdgeInsets.all(45.0),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'has presionado el boton:',
-              ),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-              Row(
-                children: botonesDePie,
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  List<Widget> get botonesDePie {
-    return [
-      TextButton(onPressed: _incrementCounter, child: const Icon(Icons.add)),
-      TextButton(onPressed: _decrementCounter, child: const Icon(Icons.remove)),
-      TextButton(
-          onPressed: _restartCounter, child: const Icon(Icons.exposure_zero)),
-    ];
   }
 }
---
+++ lib/pages/my_home_page.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
+
+  void _restartCounter() {
+    setState(() {
+      _counter = 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      //persistentFooterButtons: botonesDePie,
+      body: Card(
+        color: Colors.green,
+        margin: const EdgeInsets.all(45.0),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(30),
+            child: Column(mainAxisSize: MainAxisSize.min, children: [
+              const Text(
+                'titulo cosa xd',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              const Text(
+                'Has presionado el boton estas veces:',
+              ),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Row(children: botonesDePie),
+            ]),
+          ),
+        ),
+      ),
+    );
+  }
+
+  List<Widget> get botonesDePie {
+    return [
+      TextButton(onPressed: _incrementCounter, child: const Icon(Icons.add)),
+      TextButton(onPressed: _decrementCounter, child: const Icon(Icons.remove)),
+      TextButton(
+          onPressed: _restartCounter, child: const Icon(Icons.exposure_zero)),
+    ];
+  }
+}
