0185d56675e04972584b8bb76c9beb79d708075c
gekicchi <oyarzunm18@gmail.com>
2025-05-10 17:15:00 -0400

    borre el logger

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,12 @@
 import 'package:flutter/material.dart';
 //import 'package:flutter_svg/flutter_svg.dart';
-import 'package:logger/logger.dart';
 import 'pages/my_home_page.dart';
 
 //const String catIcon =
 //    'C:/Users/oyarz/Desktop/moviles/3479A321_2022479016/assets/icons/cat_paw.svg';
 
-var logger = Logger(
-  printer: PrettyPrinter(),
-);
-
-var loggerNoStack = Logger(
-    printer: PrettyPrinter(
-  methodCount: 0,
-));
-
 void main() {
-  demo();
   runApp(const MyApp());
-}
-
-void demo() {
-  logger.d('Log message with 2 methods');
-
-  loggerNoStack.i('Info message');
-
-  loggerNoStack.w('Just a warning!');
-
-  logger.e('Error! Something bad happened', error: 'Test Error');
-
-  loggerNoStack.t({'key': 5, 'value': 'something'});
-
-  Logger(printer: SimplePrinter(colors: true)).t('boom');
 }
 
 class MyApp extends StatelessWidget {
